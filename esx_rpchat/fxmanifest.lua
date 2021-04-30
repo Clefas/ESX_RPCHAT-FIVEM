fd189604812c7e564f60498efa4fae05a82ad422
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-04-30 03:00:49 -0400

    Feat: Remove files and fxmanifest

--- esx_rpchat/fxmanifest.lua
+++ esx_rpchat/fxmanifest.lua
@@ -1,4 +1,6 @@
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+fx_version 'cerulean'
+
+game { 'gta5' }
 
 description 'ESX RP Chat'
 
@@ -7,24 +9,16 @@ version '1.3.1'
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
 	'@es_extended/locale.lua',
-	'locales/sv.lua',
 	'locales/en.lua',
 	'locales/es.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/cs.lua',
 	'config.lua',
 	'server/main.lua'
 }
 
 client_scripts {
 	'@es_extended/locale.lua',
-	'locales/sv.lua',
 	'locales/en.lua',
 	'locales/es.lua',
-	'locales/fi.lua',
-	'locales/fr.lua',
-	'locales/cs.lua',
 	'config.lua',
 	'client/main.lua'
 }
